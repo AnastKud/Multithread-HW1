e1a4768dc8860495a070993c6addc8ea51b7d4b6
Anastasija Kudriavceva <anastasija.kudriavceva@gmail.com>
2026-03-29 17:23:34 +0300

    add command runner

---
+++ CicdRunner/CicdRunner/Services/CommandRunner.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+public class CommandRunner
+{
+    public int Run(string command, string args, string workingDir, Logger logger)
+    {
+        var process = new Process();
+
+        process.StartInfo.FileName = command;
+        process.StartInfo.Arguments = args;
+        process.StartInfo.WorkingDirectory = workingDir;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.StartInfo.UseShellExecute = false;
+
+        try
+        {
+            process.Start();
+
+            var output = process.StandardOutput.ReadToEnd();
+            var error = process.StandardError.ReadToEnd();
+
+            process.WaitForExit();
+
+            if (!string.IsNullOrWhiteSpace(output))
+                logger.Info(output.Trim());
+
+            if (!string.IsNullOrWhiteSpace(error))
+                logger.Error(error.Trim());
+
+            return process.ExitCode;
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex.Message);
+            return -1;
+        }
+    }
+}
