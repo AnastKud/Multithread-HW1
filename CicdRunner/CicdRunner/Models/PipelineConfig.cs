8c32a80e4776f6a8ad43352cfb648761405f5322
Anastasija Kudriavceva <anastasija.kudriavceva@gmail.com>
2026-03-29 15:58:34 +0300

    add pipeline models

---
+++ CicdRunner/CicdRunner/Models/PipelineConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class PipelineConfig
+{
+    public List<Stage> Pipeline { get; set; } = new();
+}
+
+public class Stage
+{
+    public string Name { get; set; } = "";
+    public string Command { get; set; } = "";
+    public string Args { get; set; } = "";
+    public bool StopOnFailure { get; set; }
+}
